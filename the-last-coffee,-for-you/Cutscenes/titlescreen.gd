bf89fd731d9ac7dfab63e2543c65857cb7c2c664
Franaffel <franksbass19@gmail.com>
2026-02-21 23:24:49 +0800

    menu

---
+++ the-last-coffee,-for-you/Cutscenes/titlescreen.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+@onready var title = $Title
+@onready var start_button = $StartButton
+@onready var quit_button = $QuitButton
+
+var fade_duration := 1.5   # seconds
+
+func _ready():
+    $StartButton.pressed.connect(_on_start_pressed)
+
+
+func _on_start_pressed():
+    # Prevent double clicking
+    start_button.disabled = true
+    quit_button.disabled = true
+
+    # Create tween
+    var tween = create_tween()
+    tween.set_parallel(true)
+
+    # Fade everything out
+    tween.tween_property(title, "modulate:a", 0.0, fade_duration)
+    tween.tween_property(start_button, "modulate:a", 0.0, fade_duration)
+    tween.tween_property(quit_button, "modulate:a", 0.0, fade_duration)
+
+    # Wait until finished
+    await tween.finished
+
+    get_tree().change_scene_to_file("res://your_scene_here.tscn")
